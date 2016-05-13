2d62882b3585172542553b50e2e54edbd07d9f69
Brent Martin <bmart35@gmail.com>
2016-05-13 14:10:21 -0500

    removed completion:boolean from evaluations

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,2 +1,6 @@
 class SurveysController < ApplicationController
+
+  def show
+  end
+
 end
--- db/migrate/20160513183624_create_evaluations.rb
+++ db/migrate/20160513183624_create_evaluations.rb
@@ -2,7 +2,6 @@ class CreateEvaluations < ActiveRecord::Migration
   def change
     create_table :evaluations do |t|
       t.float :answer
-      t.boolean :completion
       t.belongs_to :question, index: true, foreign_key: true
 
       t.timestamps null: false

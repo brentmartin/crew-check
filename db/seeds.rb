7f3cdc0b3dbf41f02f92ad586f2be8076af0f9de
Brent Martin <bmart35@gmail.com>
2016-05-13 14:27:41 -0500

    Created seed data for surveys, assessments, questions, and evaluations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+# Assessments
+# Questions
+# Evaluations
+4.times do
+  assess = Assessment.create(title:Faker::Name.title, description:Faker::Company.catch_phrase)
+  5.times do
+    @question = Question.create(body:Faker::Company.bs + "?", assessment_id: assess.id)
+    if @question.save
+      Evaluation.create(question_id: @question.id)
+    end
+  end
+end
+
+# Surveys
+8.times do
+  Survey.create(heading: Faker::StarWars.quote, intro: Faker::Hipster.paragraph(rand(2..5)), assessment_id: rand(1..Assessment.count))
+end

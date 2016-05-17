e868e72c77c49e7728316b1dd13db9cedf1dd212
Brent Martin <bmart35@gmail.com>
2016-05-17 00:20:01 -0500

    added a results page, including an average_answer_for_survey method in the question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,19 @@
 class Question < ActiveRecord::Base
   belongs_to :assessment
   has_many :evaluations
+
+  def average_answer_for_survey(survey)
+    # get all completed surveys from survey
+    cs = survey.completed_surveys
+    # get all evaluations from completed surveys
+    all_evals = cs.collect { |comp_s| comp_s.evaluations }
+    # get only evaluations with a question_id of self.id
+    evals_for_qid = all_evals.flatten.uniq.select { |e| e.question_id == self.id }
+    # sum the evaluations and
+    answer_sum = evals_for_qid.sum(&:answer)
+    # divide by the count
+    answer_count = evals_for_qid.count
+
+    average = answer_sum / answer_count
+  end
 end

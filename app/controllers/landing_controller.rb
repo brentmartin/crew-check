9af58709b7953378d9d250afaeae6780ad00b349
Brent Martin <bmart35@gmail.com>
2016-05-23 23:19:08 -0500

    created root routes per signed-in/signed-out and set before action

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,5 +1,10 @@
 class LandingController < ApplicationController
+  before_action :require_login, only: [:new]
+
   def index
     @surveys = Survey.all
+  end
+
+  def new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   resources :surveys, only: [:new, :create, :show]
   resources :completed_surveys, only: [:new, :create, :show]
+  resources :landing, only: [:new]
+
+  constraints Clearance::Constraints::SignedIn.new do
+    root to: 'landing#index', as: :signed_in_root
+  end
+
+  constraints Clearance::Constraints::SignedOut.new do
+    root to: 'landing#new'
+  end
 
   root to: "landing#index"
 end

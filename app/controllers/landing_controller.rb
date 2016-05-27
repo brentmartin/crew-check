4470927b2bfb1df88291a2dbb2ddd188d83e504c
Brent Martin <bmart35@gmail.com>
2016-05-27 16:21:24 -0500

    Changed index to only show the surveys that the current user has created

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -2,7 +2,7 @@ class LandingController < ApplicationController
   before_action :require_login, only: [:new]
 
   def index
-    @surveys = Survey.all
+    @surveys = Survey.where(user_id: current_user)
   end
 
   def new
